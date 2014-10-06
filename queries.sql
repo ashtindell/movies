6397e8366b0c9837db5c07074328dbf910ba6d1f
ashtindell <tindell.ea@gmail.com>
2014-10-06 10:30:08 -0400

    Rearranged layout on queries.sql

--- queries.sql
+++ queries.sql
@@ -3,10 +3,23 @@ SELECT * FROM movies WHERE director_id = 2;
 
 
 -- select all movies starring Leonardo DeCaprio
-SELECT name, released FROM movies INNER JOIN movies_actors ON movies.id = movies_actors.movie_id INNER JOIN actors ON actors.id =  movies_actors.actor_id WHERE actors.last='DeCaprio';
+SELECT name, released 
+FROM movies 
+INNER JOIN movies_actors ON movies.id = movies_actors.movie_id 
+INNER JOIN actors        ON actors.id = movies_actors.actor_id 
+WHERE actors.last ='DeCaprio';
 
 -- select all movies directed by S.S & starring Roy Scheider
-SELECT movies.name, actors.first, actors.last, directors.first, directors.last FROM movies INNER JOIN movies_actors ON movies.id = movies_actors.movie_id INNER JOIN actors ON actors.id =  movies_actors.actor_id INNER JOIN directors ON movies.director_id = directors.id WHERE directors.last='Spielberg' AND actors.last = 'Scheider';
+SELECT movies.name, actors.first, actors.last, directors.first, directors.last 
+FROM movies 
+INNER JOIN movies_actors ON movies.id            = movies_actors.movie_id 
+INNER JOIN actors        ON actors.id            = movies_actors.actor_id 
+INNER JOIN directors     ON directors.id         = movies.director_id
+WHERE directors.last='Spielberg' AND actors.last = 'Scheider';
 
 -- select all actors in movie 'Forrest Gump'
-SELECT movies.name, actors.first, actors.last FROM movies INNER JOIN movies_actors ON movies.id = movies_actors.movie_id INNER JOIN actors ON actors.id =  movies_actors.actor_id WHERE movies.name = 'Forrest Gump';
+SELECT movies.name, actors.first, actors.last 
+FROM movies 
+INNER JOIN movies_actors ON movies.id = movies_actors.movie_id 
+INNER JOIN actors        ON actors.id = movies_actors.actor_id 
+WHERE movies.name = 'Forrest Gump';
